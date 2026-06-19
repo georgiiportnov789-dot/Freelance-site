a638eedded23d4813ca6bd053416e6fa5d0a1c1f
georgiiportnov789-dot <georgiiportnov789@gmail.com>
2026-06-19 23:35:56 +0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ static/js/shleif_cursor.js
@@ -0,0 +1,33 @@
+let flag = 0;
+function fl1() {
+  flag = 1;
+}
+function fl0() {
+  flag = 0;
+}
+oldcordx = 0;
+oldcordy = 0;
+document.addEventListener("mousemove", (e) => {
+  if (flag == 0) {
+    return;
+  }
+  cordx = e.clientX;
+  cordy = e.clientY;
+  len = Math.sqrt(
+    (cordy - oldcordy) * (cordy - oldcordy) +
+      (cordx - oldcordx) * (cordx - oldcordx),
+  );
+  if (len > 20) {
+    let img = document.createElement("img");
+    img.src = `../static/media/shleif_foto/${Math.floor(Math.random() * 13) + 1}.png`;
+    img.classList.add("spawned-photo");
+    img.style.left = cordx + "px";
+    img.style.top = cordy + "px";
+    document.body.appendChild(img);
+    setTimeout(() => {
+      img.remove();
+    }, 2000);
+    oldcordx = cordx;
+    oldcordy = cordy;
+  }
+});
