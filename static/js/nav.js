4330a2f8eafb882a865b0e35aa50a1784be53805
georgiiportnov789-dot <georgiiportnov789@gmail.com>
2026-06-29 02:46:18 +0300

    fix: исправить пути к изображениям в навигационной панели

--- static/js/nav.js
+++ static/js/nav.js
@@ -22,9 +22,9 @@ class NavbarComponent extends HTMLElement {
                     </div>
                 </div>
                 <ul class="nav__list">
-                    <li class="nav__item"><a class="nav__link" href="/main"><span class="nav__link-text">Основное</span><img class="nav__link-icon" src="../static/media/navbar/основное.png" alt=""></a></li>
-                    <li class="nav__item"><a class="nav__link" href="/orders"><span class="nav__link-text">Заказы</span><img class="nav__link-icon" src="../static/media/navbar/заказы.png" alt=""></a></li>
-                    <li class="nav__item"><a class="nav__link" href="/applications"><span class="nav__link-text">Заявки</span><img class="nav__link-icon" src="../static/media/navbar/заявки.png" alt=""></a></li>
+                    <li class="nav__item"><a class="nav__link" href="/main"><span class="nav__link-text">Основное</span><img class="nav__link-icon" src="../static/media/navbar/Основное.png" alt=""></a></li>
+                    <li class="nav__item"><a class="nav__link" href="/orders"><span class="nav__link-text">Заказы</span><img class="nav__link-icon" src="../static/media/navbar/Заказы.png" alt=""></a></li>
+                    <li class="nav__item"><a class="nav__link" href="/applications"><span class="nav__link-text">Заявки</span><img class="nav__link-icon" src="../static/media/navbar/Заявки.png" alt=""></a></li>
                     <li class="nav__item"><a class="nav__link" href="/my_profile"><span class="nav__link-text">Мой профиль</span><img class="nav__link-icon" src="../static/media/navbar/Мой профиль.png" alt=""></a></li>
                     <li class="nav__item"><a class="nav__link" href="/about"><span class="nav__link-text">О нас</span><img class="nav__link-icon" src="../static/media/navbar/О нас.png" alt=""></a></li>
                 </ul>
